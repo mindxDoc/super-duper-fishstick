c29b8f875f8ddd802ef410ff46f0faca708a47ff
Phuoc Nguyen Dinh <dolphin@Mac-Khong-Khi-cua-Phuoc.local>
2023-11-08 11:09:11 +0700

    change api

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = "https://book-review-gamma.vercel.app"
+axios.defaults.baseURL = "https://book-review-gamma.onrender.com"
 
 root.render(
   <App />
